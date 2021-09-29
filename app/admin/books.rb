409e2f8a708b885d53b288e0d2276081bb9e31dc
dantes47 <dantes817@gmail.com>
2021-09-29 22:33:11 +0300

    was added resources into active admin

---
+++ app/admin/books.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Book do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :title, :description, :author, :coauthor, :user_id, :category_id
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:title, :description, :author, :coauthor, :user_id, :category_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
